18df6c1783f55d73a25faa02401e695dfc27dcad
noise23 <admin@acidpool.com>
2016-01-30 00:18:22 +0200

    Revert "update paths"
    This reverts commit b5ba12af583bed5aabe4a4e757bd71ad4afc2e79.

--- dev-util/get_block.js
+++ dev-util/get_block.js
@@ -5,7 +5,7 @@ var util = require('util');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var RpcClient = require('../node_modules/core/RpcClient');
+var RpcClient = require('../node_modules/phscore/RpcClient');
 
 var config = require('../config/config');
 
